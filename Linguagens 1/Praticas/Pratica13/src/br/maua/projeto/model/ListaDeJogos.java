0132cf3d6b1c28bbf2ec11a3bfc4a851c1c0896b
Daniel-xavier <danielughinix@hotmail.com>
2020-08-24 16:08:59 -0300

    Pratica json

--- Linguagens 1/Praticas/Pratica13/src/br/maua/projeto/model/ListaDeJogos.java
+++ Linguagens 1/Praticas/Pratica13/src/br/maua/projeto/model/ListaDeJogos.java
@@ -4,23 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListaDeJogos {
-    private List<Jogo> minhaListadeJogos;
+    private List<Jogo> minhaListaDeJogos;
 
     public ListaDeJogos() {
-        this.minhaListadeJogos = new ArrayList<>();
+        this.minhaListaDeJogos = new ArrayList<>();
     }
 
     public void addJogo(Jogo jogo){
-        this.minhaListadeJogos.add(jogo);
+        this.minhaListaDeJogos.add(jogo);
     }
 
     @Override
     public String toString() {
         return "ListadeJogos{" +
-                "minhaListadeJogos=" + minhaListadeJogos +
+                "minhaListadeJogos=" + minhaListaDeJogos +
                 '}';
     }
     public final List<Jogo> getJogos() {
         return this.minhaListaDeJogos;
     }
+
 }
