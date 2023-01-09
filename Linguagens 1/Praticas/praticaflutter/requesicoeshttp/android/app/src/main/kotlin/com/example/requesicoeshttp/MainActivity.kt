a83bf9e3baf3710007fcf3eff06205ad285a08a6
Daniel-Xavier <danielughinix@hotmail.com>
2023-01-09 17:12:56 -0300

    Limpando repositorio

--- Linguagens 1/Praticas/praticaflutter/requesicoeshttp/android/app/src/main/kotlin/com/example/requesicoeshttp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.requesicoeshttp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Linguagens 1/Projetos/testeflutter/android/app/src/main/kotlin/com/example/testeflutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.testeflutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
